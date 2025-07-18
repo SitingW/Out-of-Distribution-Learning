dcba56cd659bd75d06f5b6aba07fae1f0fb6c06b
SitingW <siting3@ualberta.ca>
2025-07-18 15:18:23 -0600

    removed the bias term from the linear regression model

--- torch_modelling/src/models/linear_model.py
+++ torch_modelling/src/models/linear_model.py
@@ -6,7 +6,11 @@ class LinearModel(nn.Module):
         super().__init__()
         self.input_channels = input_channels
         self.output_channels = output_channels
-        self.linear_layer = nn.Linear(input_channels, output_channels)
+        self.linear_layer = nn.Linear(input_channels, output_channels, bias=False)
+
+        #check if I have no biased term
+        print("Has bias:", self.linear_layer.bias is not None)  # False
+        print("Bias:", self.linear_layer.bias)    
     
     def forward(self, x) -> torch.Tensor:
        return self.linear_layer(x)
