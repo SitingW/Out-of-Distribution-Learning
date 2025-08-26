2513553c494152f48f71a7b11984af452b36e96c
SitingW <siting3@ualberta.ca>
2025-08-26 14:41:54 -0600

    Update python_env.sh. output and error file added

--- torch_modelling/scripts/python_env.sh
+++ torch_modelling/scripts/python_env.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#SBATCH --account=def-someuser
+# SBATCH --account=def-someuser (what is the account I should use?)
 #SBATCH --mem-per-cpu=1500M      # increase as needed
 #SBATCH --time=1:00:00
+#SBATCH --job-name=python_env
+#SBATCH --output=python_env_%j.out
+#SBATCH --error=python_env_%j.err
 
 module load python/3.13
 virtualenv --no-download $SLURM_TMPDIR/env
